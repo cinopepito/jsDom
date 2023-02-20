38276240e9f7832df8831374405ca75dfb444071
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-02-20 19:08:13 +0100

    adding some note in notes folder

--- index.js
+++ index.js
@@ -2,18 +2,29 @@
 
 //click event
 const questionContainer = document.querySelector(".click-event");
-console.log(questionContainer);
-//add class method when you perform a click
-questionContainer.addEventListener("click", () => {
-  questionContainer.classList.add("question-click");
-});
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const response = document.querySelector(".rep");
+
+// questionContainer.addEventListener("click", () => {
+//   questionContainer.classList.add("question-click");
+// });
 //to add a class to the element, we can use a method like .classList.add
 //to remove a class to the element, we can use a method like .classList.remove
-questionContainer.addEventListener("click", () => {
-  questionContainer.classList.add("question-click");
-});
+// questionContainer.addEventListener("click", () => {
+//   questionContainer.classList.add("question-click");
+// });
 
 //sometimes developper can use toggle to show the element and to hide it when we want the element to pop up
 questionContainer.addEventListener("click", () => {
-  questionContainer.toggle("question-click");
+  questionContainer.classList.toggle("question-click");
+});
+
+btn1.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "green";
+});
+btn2.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "green";
 });
--- notes.md
+++ notes.md
@@ -2,3 +2,7 @@ In javascript, we have selector
 Selector is tools that we used to select element on the dom to manipulate them.
 we have querySelector, we have getelemntby Id or by tagname or byclassname.
 We fetch the element and then we modify the element but there is something really important about that. We need to check if our selection was well made. That takes 5 minutes but prevent a lot of trouble.
+
+we use classlist.add to add a class
+we use classlist.remove to remove a class
+we use toogle to swaap between classes, when the class exist, the toggle method remove that and when it doe not exist, it apply that class, this method is very useful when creating dynamic website with javascript program.
